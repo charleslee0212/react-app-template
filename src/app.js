b44d52a1668ace7d42d55db0b7c7b22c52ae4229
charles <63661051+charleslee0212@users.noreply.github.com>
2024-07-26 00:59:59 -0600

    test component

--- src/app.js
+++ src/app.js
@@ -1,8 +1,9 @@
 import React from "react";
+import TestComponent from "./components/TestComponent";
 import './styles/app.css';
 
 const App = () => {
-    return <div>Hello World!</div>
+    return <TestComponent/>
 };
 
 export default App;
